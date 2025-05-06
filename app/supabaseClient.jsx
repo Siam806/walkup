065a8c97c3c47d018b4375084552ef0b3491c03c
Siam Ngamkam <siamthedoom@gmail.com>
2025-05-06 15:16:26 +0200

    feat: switch to environment variables for Supabase URL and Key for enhanced security

--- app/supabaseClient.jsx
+++ app/supabaseClient.jsx
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://aqsgngquzxlqmrpcevkh.supabase.co"; // Extracted from your connection string
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxc2duZ3F1enhscW1ycGNldmtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNTIxOTgsImV4cCI6MjA2MTkyODE5OH0.C3KatBnHwAM8CUzOlRGfIMggkX494YQ7gr5vJelJXx4"; // Replace with your actual anon key from the Supabase dashboard
+// Use environment variables for Supabase URL and Key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
